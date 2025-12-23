7d3c2637aed796b1e348ec37cc8c29ffd614959e
Ruslan-Madaminov <russellmad@yandex.ru>
2025-12-23 15:08:52 +0300

    cookies text updated

--- src/components/popups/popups.comp.jsx
+++ src/components/popups/popups.comp.jsx
@@ -83,8 +83,8 @@ export const PopupFilter = () => {
 
 export const PopupCookies = () => {
   return <PopupsLayout data={'popup_for_cookies'} btnClose={false}>
-    <strong>Сайт использует Cookies</strong>
-    <p>Без них всё работало бы хуже</p>
+    <strong>Этот сайт использует cookies</strong>
+    <p>Чтобы улучшить работу сайта, мы собираем данные о ваших посещениях. Оставаясь на сайте, вы соглашаетесь с нашей политикой использования cookies.</p>
     <button class="button button-filled button-primary" data-closepopup="" data-accept="">
       <span>Хорошо!</span>
     </button>
