5e1c80a06c2ab8dd6fdeb3af9fce5f52092f1ab8
Ruslan-Madaminov <russellmad@yandex.ru>
2025-12-24 11:04:26 +0300

    поправка ссылок в шапке, убрал у всех .html/ и из адресов и добавил новые ссылки

--- src/components/nav/navigation.js
+++ src/components/nav/navigation.js
@@ -1,26 +1,32 @@
 export const links = [
   {
     label: "Собственникам",
-    href: "/to-owners.html/",
+    href: "/to-owners",
     children: [
-      { label: "Котловой метод", href: "/to-owners/boiler-method.html/" },
-      { label: "Памятка инвестору", href: "/to-owners/memo-to-investor.html/" },
-      { label: "Калькулятор доходности", href: "/to-owners/profitability-calculator.html/" },
-      { label: "Личный кабинет", href: "/to-owners/lk.html/" },
-      { label: "Услуги", href: "/to-owners/services.html/" },
+      { label: "Котловой метод", href: "/to-owners/boiler-method" },
+      { label: "Памятка инвестору", href: "/to-owners/memo-to-investor" },
+      { label: "Калькулятор доходности", href: "/to-owners/profitability-calculator" },
+      { label: "Личный кабинет", href: "/to-owners/lk" },
+      { label: "Услуги", href: "/to-owners/services" },
     ],
   },
   {
     label: "Передача в управление",
-    href: "/transfer-to-management.html/",
+    href: "/transfer-to-management",
   },
   {
     label: "Курорты",
-    href: "/resorts.html/",
+    href: "/resorts",
   },
   {
     label: "Для бизнеса",
-    href: "/for-business.html/",
+    href: "/for-business",
+    children: [
+      { label: "Туроператорам", href: "/to-tour-operators" },
+      { label: "Поставщикам", href: "/to-suppliers" },
+      { label: "Сотрудничество", href: "/cooperation" },
+      { label: "Реклама на курортах", href: "/advertising-at-resorts" }
+    ]
   },
   {
     label: "О компании",
@@ -28,6 +34,6 @@ export const links = [
   },
   {
     label: "Купить недвижимость",
-    href: "/buy-real-estate.html/",
+    href: "/buy-real-estate",
   },
 ];
