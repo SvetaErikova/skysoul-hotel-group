2bdaf6b1c9a1c04fff927d2d311449dc6293cca2
Ruslan-Madaminov <russellmad@yandex.ru>
2025-12-24 09:15:14 +0300

    актуализация футере по контенту

--- src/components/form/form-subscription.jsx
+++ src/components/form/form-subscription.jsx
@@ -4,14 +4,42 @@ import {Button} from "../_ui/button/button.jsx";
 
 export const Form_subscription = () =>{
   return (
-      <div class="form">
-        <div class="form--subtitle">Подписаться на получение актуальных новостей</div>
-        <div class="form--subscription">
-          <Input type={"text"} name={"name"} placeholder={"Иванов Иван Иванович"} required={true} autocomplete={"autocomplete"}></Input>
-          <Button type={'link'} color={'primary'} htmlType={'submit'} dark={true} icon={'icon-arrow-right'}></Button>
-        </div>
-        <Form_policy></Form_policy>
-        <Form_policy></Form_policy>
+    <form class="form">
+      <div class="form--subtitle">Подписаться на получение актуальных новостей</div>
+      <div class="form--subscription">
+        <Input type={"email"}
+               name={"name"}
+               placeholder={"Ваш адрес электронной почты example@skygrouphotel.ru"}
+               required={true}
+               autocomplete={"autocomplete"}></Input>
+        <Button type={'link'}
+                color={'primary'}
+                htmlType={'submit'}
+                dark={true}
+                icon={'icon-arrow-right'}></Button>
       </div>
+      <div className="form--policy">
+        <div className="form--input">
+          <label>
+            <input name="policy-agree" type="checkbox" value="" required="required"/>
+            <div className="checkbox"></div>
+            <span>
+              Я даю согласие на&nbsp;<a href="https://skygrouphotel.ru/policy-doc.html" target="_blank" rel="noopener">обработку персональных данных</a>
+            </span>
+          </label>
+        </div>
+      </div>
+      <div className="form--policy">
+        <div className="form--input">
+          <label>
+            <input name="adv-agree" type="checkbox" value="" required="required"/>
+            <div className="checkbox"></div>
+            <span>
+              Я даю согласие на&nbsp;<a href="https://skygrouphotel.ru/adv-doc.html" target="_blank" rel="noopener">получение рассылки рекламно-информационных материалов</a>
+            </span>
+          </label>
+        </div>
+      </div>
+    </form>
   )
 }
